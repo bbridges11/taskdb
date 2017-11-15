185a5c82a6486dc42a838514485419fe1a156fd1
Kelvin Fichter <kef50@case.edu>
2017-11-15 12:18:43 -0500

    Updated README.md

--- taskdb/db.py
+++ taskdb/db.py
@@ -1,48 +1,6 @@
-import time
-import traceback
 import multiprocessing
-import multiprocess
-
-def run_until_complete(target=None, args=(), interval=0.01):
-    result = None
-    while (not result or (isinstance(result, tuple) and not all(result)) and not (isinstance(result, list) and not result)):
-        try:
-            result = target(*args)
-        except Exception:
-            error = traceback.format_exc()
-            print(error)
-        time.sleep(interval) # Avoid busy waiting
-    return result
-
-class Task(object):
-    def __init__(self, task_id, function, arguments=()):
-        self.task_id = task_id
-        self.function = function
-        self.arguments = arguments
-
-        self.manager = multiprocessing.Manager()
-        self.namespace = self.manager.Namespace()
-        self.namespace.data = None
-        self.watchers = 0
-        self.active = False
-        self.process = None
-
-    def execute(self):
-        while True:
-            self.namespace.data = run_until_complete(target=self.function, args=self.arguments)
-            time.sleep(0.01)
-
-    def get_data(self):
-        return self.namespace.data
-
-    def start(self):
-        self.active = True
-        self.process = multiprocessing.Process(target=self.execute)
-        self.process.start()
-
-    def stop(self):
-        self.active = False
-        self.process.terminate()
+import time
+from taskdb.task import Task
 
 class TaskDB(object):
     def __init__(self):
@@ -58,7 +16,6 @@ class TaskDB(object):
             task = self.tasks.get(task_id, None)
             if not task:
                 task = self.add_task(task_id, function, arguments)
-                print('ADDED TASK')
             if not task.active:
                 task.start()
             task.watchers += 1
