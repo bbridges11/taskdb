38ae9ab01eb1b889d19b751ab16026747b8b55ec
Kelvin Fichter <kef50@case.edu>
2017-11-15 12:06:00 -0500

    Added gitignore

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='taskdb',
     version='0.0.1',
-    description='TaskDB',
+    description='Task DB',
     long_description=long_description,
     url='https://github.com/kfichter/taskdb',
     author='Kelvin Fichter',
